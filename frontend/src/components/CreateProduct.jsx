8e435903cd472f56bf44dd6a75e6cc6b0c950b4e
msa-developer <shoaib9542186823@gmail.com>
2026-05-18 10:08:20 +0530

    fixing the working of form

--- frontend/src/components/CreateProduct.jsx
+++ frontend/src/components/CreateProduct.jsx
@@ -1,18 +1,65 @@
-import { CircleX } from "lucide-react";
+import { CircleX, DollarSign } from "lucide-react";
 import useProductStore from "../zustand/useProductStore";
 
 const CreateProduct = () => {
-  const { CreateProduct } = useProductStore();
+  const { CreateProduct, formData, setFormData } = useProductStore();
 
   return (
     <dialog id="modal" className="modal">
       <main className="modal-box">
-        <form method="dialog" onSubmit={CreateProduct}>
+        <form method="dialog">
           <button className="btn btn-circle btn-soft btn-ghost absolute right-2 top-2">
             <CircleX />
           </button>
         </form>
-        <h3>Add Product</h3>
+        <form
+          onSubmit={CreateProduct}
+          className="flex flex-col w-full space-y-4"
+        >
+          <legend className="fieldset-legend">Add Product</legend>
+
+          <label className="label font-bold">Name</label>
+          <input
+            type="text"
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+            value={formData.name}
+            className="input w-full"
+            placeholder="product name..."
+          />
+
+          <label className="label font-bold">Image</label>
+          <input
+            type="text"
+            value={formData.image}
+            className="input w-full"
+            placeholder="image..."
+            onChange={(e) =>
+              setFormData({ ...formData, image: e.target.value })
+            }
+          />
+
+          <label className="label font-bold">
+            Price <DollarSign />{" "}
+          </label>
+          <input
+            type="number"
+            value={formData.price}
+            className="input w-full"
+            placeholder="0.00"
+            onChange={(e) =>
+              setFormData({ ...formData, price: e.target.value })
+            }
+          />
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+          >
+            Add
+          </button>
+        </form>
       </main>
     </dialog>
   );
